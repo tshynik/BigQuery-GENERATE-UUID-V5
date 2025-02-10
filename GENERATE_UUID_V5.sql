40f6461ce98fa84af4cadb2858e94101ac590d91
Akiva Wolberg <15022566+tshynik@users.noreply.github.com>
2025-02-10 12:22:02 -0500

    reworked as a permanent user-defined function in BigQuery

--- GENERATE_UUID_V5.sql
+++ GENERATE_UUID_V5.sql
@@ -1,25 +1,40 @@
-DECLARE NAMESPACE_OID_IN_BYTES BYTES; 
-SET NAMESPACE_OID_IN_BYTES = b'k\xa7\xb8\x12\x9d\xad\x11\xd1\x80\xb4\x00\xc0O\xd40\xc8';
-
-CREATE TEMP FUNCTION FORMAT_UUID(UUID STRING)
-  RETURNS STRING
-  AS (
-    CONCAT(SUBSTR(UUID,1,8), "-", SUBSTR(UUID,9,4), "-", SUBSTR(UUID,13,4), "-", SUBSTR(UUID,17,4), "-", SUBSTR(UUID,21,12))
-  );
-
-CREATE TEMP FUNCTION GENERATE_UUID_V5(Reference STRING, namespace_in_bytes BYTES)
-  RETURNS STRING
-  AS (
-    FORMAT_UUID(
-      TO_HEX(
-        CAST(
-          TO_HEX(LEFT(SHA1(CONCAT(namespace_in_bytes, CAST(Reference AS BYTES FORMAT 'UTF8'))), 16)) AS BYTES FORMAT 'HEX') & CAST('ffffffffffff0fff3fffffffffffffff' AS BYTES FORMAT 'HEX') | CAST('00000000000050008000000000000000' 
-        AS BYTES FORMAT 'HEX')
-      )
+CREATE FUNCTION dataset.GENERATE_UUID_V5(reference_var STRING) RETURNS STRING AS (
+    -- full query wrapped in parens thanks to this BigQuery error message:
+    -- "The body of each CREATE FUNCTION statement is an expression, not a query;
+    -- to use a query as an expression, the query must be wrapped with additional
+    -- parentheses to make it a scalar subquery expression"
+    (
+        WITH generated_uuid AS (
+            SELECT
+                TO_HEX(
+                    CAST(
+                        TO_HEX(
+                            LEFT(
+                                SHA1(
+                                    CONCAT(
+                                        -- namespace in bytes: OID
+                                        b'k\xa7\xb8\x12\x9d\xad\x11\xd1\x80\xb4\x00\xc0O\xd40\xc8',
+                                        CAST(reference_var AS BYTES FORMAT 'UTF8')
+                                    )
+                                ), 16)
+                        ) AS BYTES FORMAT 'HEX'
+                    )
+                    & CAST('ffffffffffff0fff3fffffffffffffff' AS BYTES FORMAT 'HEX')
+                    | CAST('00000000000050008000000000000000' AS BYTES FORMAT 'HEX')
+              ) AS uuid
+        )
+        SELECT CONCAT(
+            SUBSTR(uuid,1,8), "-",
+            SUBSTR(uuid,9,4), "-",
+            SUBSTR(uuid,13,4), "-",
+            SUBSTR(uuid,17,4), "-",
+            SUBSTR(uuid,21,12)
+            )
+        FROM generated_uuid
     )
-  );
+);
 
 SELECT 
-  reference,
-  GENERATE_UUID_V5(reference, NAMESPACE_OID_IN_BYTES) AS UUID
-  FROM UNNEST(["123","123456"]) AS reference;
+    reference_var,
+    dataset.GENERATE_UUID_V5(reference_var) AS uuid
+FROM UNNEST(['123','123456']) AS reference_var;
